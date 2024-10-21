ece3868f54299eacc5e35ff81028af250d90b375
Your Name <you@example.com>
2024-10-21 00:37:43 -0300

    dont send inside async task

--- app/sidekiq/send_article.rb
+++ app/sidekiq/send_article.rb
@@ -5,8 +5,6 @@ class SendArticle
 
   def perform(input_data)
     input_s = InputStruct.new(**input_data)
-
-    response = HTTParty.post("http://localhost:8080", body: input_s.to_json)
     puts response.code
     puts response.body
   end
